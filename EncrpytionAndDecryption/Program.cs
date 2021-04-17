37f62c4f3648fcd56daa1b3aeb447b058c2cad15
Evin Watson <evinwatson@outlook.com>
2021-04-16 17:25:26 -0700

    Added Func

--- EncrpytionAndDecryption/Program.cs
+++ EncrpytionAndDecryption/Program.cs
@@ -7,8 +7,18 @@ namespace EncrpytionAndDecryption
         public static string Alphabet = "abcdefghijklmnopqrstuvwxyz";
         static void Main(string[] args)
         {
-            Console.WriteLine(SingleKeyEncrypt("we attack at dawn", "c"));
-            Console.WriteLine(MultiKeyEncrypt("we attack at dawn", "cat"));
+            Console.WriteLine("Enter the message you want to encrypt: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine("Input key for single-key encryption(single character a-z):");
+            string key = Console.ReadLine();
+            Console.WriteLine($"'{word}' encrypted using single key ({key}): {SingleKeyEncrypt(word, key)}");
+            Console.WriteLine($"'{word}' decrypted using single key ({key}): {SingleKeyDecrypt(SingleKeyEncrypt(word, key), key)}");
+
+            Console.WriteLine("Input key for multi-key encryption(any number of characters a-z):");
+            key = Console.ReadLine();
+            Console.WriteLine($"'{word}' encrypted using multi key ({key}): {MultiKeyEncrypt(word, "cat")}");
+            Console.WriteLine($"'{word}' decrypted using multi key ({key}): {MultiKeyDecrypt(MultiKeyEncrypt(word, "cat"), "cat")}");
 
         }
 
@@ -50,6 +60,26 @@ namespace EncrpytionAndDecryption
                     {
                         int delta = j + keyNum;
                         if (delta < 0)
+                            delta += 26;
+                        if (delta > 25)
+                            delta -= 26;
+                        output += Alphabet[delta];
+                    }
+            return output;
+        }
+        public static string SingleKeyDecrypt(string input, string key)
+        {
+            input = CleanString(input);
+            int keyNum = getKeyNum(key);
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+                for (int j = 0; j < Alphabet.Length; j++)
+                    if (input[i] == Alphabet[j])
+                    {
+                        int delta = j - keyNum;
+                        if (delta < 0)
+                            delta += 26;
+                        if (delta > 25)
                             delta -= 26;
                         output += Alphabet[delta];
                     }
@@ -68,6 +98,32 @@ namespace EncrpytionAndDecryption
                         int keyNum = getKeyNum(key[keyIndex].ToString());
                         int delta = j + keyNum;
                         if (delta < 0)
+                            delta += 26;
+                        if (delta > 25)
+                            delta -= 26;
+                        output += Alphabet[delta];
+                        if (keyIndex == key.Length - 1)
+                            keyIndex = 0;
+                        else
+                            keyIndex++;
+                    }
+            return output;
+        }
+
+        public static string MultiKeyDecrypt(string input, string key)
+        {
+            input = CleanString(input);
+            string output = "";
+            int keyIndex = 0;
+            for (int i = 0; i < input.Length; i++)
+                for (int j = 0; j < Alphabet.Length; j++)
+                    if (input[i] == Alphabet[j])
+                    {
+                        int keyNum = getKeyNum(key[keyIndex].ToString());
+                        int delta = j - keyNum;
+                        if (delta < 0)
+                            delta += 26;
+                        if (delta > 25)
                             delta -= 26;
                         output += Alphabet[delta];
                         if (keyIndex == key.Length - 1)
