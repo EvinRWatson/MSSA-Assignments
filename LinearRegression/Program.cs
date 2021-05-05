652955bca1fb44c9d72149f8cf6b3851d533a51c
Evin Watson <evinwatson@outlook.com>
2021-05-05 15:29:24 -0700

    Added ParseCSV

---
+++ LinearRegression/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinearRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding Data
+            Position Andre = new Position(165, 66);
+            Position Aaron = new Position(185, 71);
+            Position James = new Position(190, 70);
+            Position Charles = new Position(210, 72);
+            Position Jacon = new Position(180, 72);
+            Position Kindra = new Position(150, 67);
+            Position Malachi = new Position(180, 68);
+
+            //User Interaction
+            Console.WriteLine("The forumula for the inputed data model is:");
+            Console.WriteLine($"Y = {Position.getAlpha()} + {Position.getBeta()} * X");
+
+            //Taking in predictor variable
+            Console.WriteLine("Please enter a value for X as a predictor:");
+            double X = double.Parse(Console.ReadLine());
+
+            //Calculating and output data
+            double result = X * Position.getBeta() + Position.getAlpha();
+            Console.WriteLine($"The result of X of {X} Y is {result}.");
+
+        }
+    }
+
+    class Position
+    {
+        //Storing positions in a list so its easier to operate on
+        public static List<Position> PositionList = new List<Position>();
+
+        // Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        //Constructors
+        public Position()
+        {
+            this.X = 0.0;
+            this.Y = 0.0;
+            PositionList.Add(this);
+        }
+        public Position(double X = 0, double Y = 0)
+        {
+            this.X = X;
+            this.Y = Y;
+            PositionList.Add(this);
+        }
+        public Position(int X = 0, int Y = 0)
+        {
+            this.X = X;
+            this.Y = Y;
+            PositionList.Add(this);
+        }
+
+        //Equations
+        public static double getAlpha() => (sumY() * sumXSquared() - sumX() * sumXY())
+                                        / (getN() * sumXSquared() - Math.Pow(sumX(), 2));
+
+        public static double getBeta() => (getN() * sumXY() - sumX() * sumY())
+                                        / (getN() * sumXSquared() - (Math.Pow(sumX(), 2)));
+
+        //Summation Functions
+        public static double sumX()
+        {
+            double sum = 0;
+            foreach (Position item in PositionList)
+                sum += item.X;
+            return sum;
+        }
+        public static double sumY()
+        {
+            double sum = 0;
+            foreach (Position item in PositionList)
+                sum += item.Y;
+            return sum;
+        }
+        public static double sumXSquared()
+        {
+            double sum = 0;
+            foreach (Position item in PositionList)
+                sum += (item.X * item.X);
+            return sum;
+        }
+        public static double sumXY()
+        {
+            double sum = 0;
+            foreach (Position item in PositionList)
+                sum += (item.X * item.Y);
+            return sum;
+        }
+
+        public static int getN() => PositionList.Count;
+    }
+}
---
+++ ParseCSV/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*************************
+        * read CSV with embedded commas
+        * parse CSV into separate fields and
+        * ignore commas within quoted string
+        * ***********************/
+        Console.WriteLine("Reading CSV with embedded commas");
+        List<string> myList = new List<string>();
+        string input1 = "\"a,b\",c";
+        myList.Add(input1);
+        string input2 = "\"Obama, Barack\",\"August 4, 1961\",\"Washington, D.C.\"";
+        myList.Add(input2);
+        string input3 = "\"Ft. Benning, Georgia\",32.3632N,84.9493W," +
+        "\"Ft. Stewart, Georgia\",31.8691N,81.6090W," +
+        "\"Ft. Gordon, Georgia\",33.4302N,82.1267W";
+        myList.Add(input3);
+        foreach (string s in myList)
+        {
+            Console.WriteLine($"Current input is {s}");
+            List<string> output = getCSV(s);
+            int len = output.Count;
+            Console.WriteLine($"This line has {len} fields. They are:");
+            foreach (string s1 in output)
+                Console.WriteLine(s1);
+        }
+    }
+
+    public static List<string> getCSV(string input)
+    {
+        List<string> outputList = new List<string>();
+        var result = Regex.Split(input, ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+        foreach (var n in result)
+        {
+            outputList.Add(n);
+        }
+        return outputList;
+
+        //List<string> outputList = new List<string>();
+        //int minIndex = 0;
+        //bool isInQuotes = false;
+
+        //for (int i = 0; i < input.Length - 1; i++)
+        //{
+        //    int counter = 0;
+
+
+        //    if (input[i] == '"' && isInQuotes == true)
+        //    {
+        //        isInQuotes = false;
+        //        continue;
+        //    }
+
+        //    if (input[i] == '"' && isInQuotes == false)
+        //    {
+        //        isInQuotes = true;
+        //        continue;
+        //    }
+
+
+        //    if (input[i].Equals(','))
+        //    {
+        //        if(!isInQuotes)
+        //        {
+        //            minIndex = i + 1;
+        //            for (int j = i + 1; j < input.Length - 1 && !input[j].Equals(','); j++)
+        //            {
+        //                counter++;
+        //            }
+        //            string newEntry = input.Substring(minIndex + 1, counter);
+        //            outputList.Add(newEntry);
+        //        }
+
+        //        if (isInQuotes)
+        //        {
+        //            minIndex = i + 1;
+        //            for (int j = i + 1; j < input.Length - 1 && !input[j].Equals('"'); j++)
+        //            {
+        //                counter++;
+        //            }
+        //            string newEntry = input.Substring(minIndex + 1, counter);
+        //            outputList.Add(newEntry);
+        //        }
+        //    }
+
+        //}
+        //return outputList;
+    }
+}
